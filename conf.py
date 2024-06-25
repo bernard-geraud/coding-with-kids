6c7eea5d146d69e04df9a40894406ee7f805ca20
bernard-geraud <bernard.dongmo@kaeyros-analytics.com>
2024-06-25 16:08:46 +0100

    configure conf.py file to handle multiple languages

--- conf.py
+++ conf.py
@@ -17,11 +17,20 @@ author = 'Devskills'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'recommonmark'
+    'recommonmark',
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.githubpages',
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
+#exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
